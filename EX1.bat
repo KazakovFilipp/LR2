be5b70bd3bc9b4a64fa4f205057e4b4ff785df39
KazakovFilipp <103320977+KazakovFilipp@users.noreply.github.com>
2022-09-26 11:46:52 +0300

    Add files via upload

---
+++ EX1.bat
@@ -0,0 +1,22 @@
+echo off
+md Kazakov
+cd Kazakov
+md Filipp
+md adb1908
+pause
+echo off
+echo > 14121999.txt
+cd adb1908
+echo > Afilla.txt
+cd..
+cd..
+pause
+echo off
+del Kazakov /S /Q /F
+pause
+echo off
+cd Kazakov
+rd Filipp
+rd adb1908
+cd..
+rd Kazakov
